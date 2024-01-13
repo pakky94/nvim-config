4d68ec4cd456478937364c322bfeea6e64d65b3f
Marco Pacchialat <m.pacchialat@gmail.com>
2024-01-13 17:19:23 +0100

    move telescope stuff & add telescope-undo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,7 +33,6 @@ return {
     tag = 'v3.9.1',
   },
   'leoluz/nvim-dap-go',
-  'nvim-telescope/telescope-dap.nvim',
   'theHamsta/nvim-dap-virtual-text',
 
   {
@@ -94,28 +93,4 @@ return {
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      --[[
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      --]]
-    },
-  },
-
 }
